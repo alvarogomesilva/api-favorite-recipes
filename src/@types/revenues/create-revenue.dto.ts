1e5dc2abe96ee3c80e50d6c014c8ce9d21c7e570
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-01-24 20:00:06 -0300

    refactor: adjustment in revenue sending

--- src/@types/revenues/create-revenue.dto.ts
+++ src/@types/revenues/create-revenue.dto.ts
@@ -3,8 +3,8 @@ export interface CreateRevenueDTO {
     title: string;
     id_user: string;
     description: string;
-    ingredients: string;
-    preparation_mode: string;
+    ingredients: string[];
+    preparation_mode: string[];
     image?: string;
     id_category: string;
 }
